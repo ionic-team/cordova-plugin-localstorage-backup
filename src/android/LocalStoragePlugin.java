4bf60a499876f5dcf058acecd620678df3a7b38e
Jeroen Maas <jeroen.maas@munisense.com>
2015-07-17 13:24:04 +0200

    Updated to be functional.
    It works by using 'loadDataWithBaseUrl' from default webView. For android
    4.4 and higher you could chose to use: 'evaluateJavascript'.

--- src/android/LocalStoragePlugin.java
+++ src/android/LocalStoragePlugin.java
@@ -11,120 +11,125 @@ import android.webkit.ValueCallback;
 import java.io.IOException;
 import java.io.StringReader;
 
-import org.apache.cordova.AndroidWebView;
+import android.webkit.WebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
+import android.app.Activity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebViewClient;
 
 /**
- * This class echoes a string called from JavaScript.
+ * LocalStoragePlugin
+ *
+ * This plugin can be used to fetch data from the native browser localStorage.
  */
 public class LocalStoragePlugin extends CordovaPlugin {
 
-  public static final String TAG = "LocalStorage";
+    public static final String TAG = "LocalStorage";
 
-  /**
-   * Constructor.
-   */
-  public LocalStoragePlugin() {
-  }
+    /**
+     * Constructor.
+     */
+    public LocalStoragePlugin() {
+    }
 
+    private CallbackContext cbContext;
 
-  private CallbackContext cbContext;
-
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    // Device.uuid = getUuid();
-  }
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
 
     /**
      * Executes the request and returns PluginResult.
      *
-     * @param action        The action to execute.
-     * @param args          JSONArry of arguments for the plugin.
-     * @param callbackId    The callback id used when calling back into JavaScript.
-     * @return              A PluginResult object with a status and message.
+     * @param action     The action to execute.
+     * @param args       JSONArry of arguments for the plugin.
+     * @param callbackId The callback id used when calling back into JavaScript.
+     * @return A PluginResult object with a status and message.
      */
-  @Override
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-
-      CordovaActivity activity = ((CordovaActivity)this.cordova.getActivity());
-      AndroidWebView defaultWebView = (AndroidWebView) activity.makeWebView();
-
-      cbContext = callbackContext;
-
-        try {
-          String jsForLocalStorage = "" +
-          "var items = [];" +
-          "for (var i = 0; i < localStorage.length; i++){" +
-            "items.push({key: localStorage.key(i), value: localStorage.getItem( localStorage.key(i) )});"+
-          "}";
-          String jsCallback = "JSInterface.passBackValues(items);";
-
-          if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-              // In KitKat+ you should use the evaluateJavascript method
-              defaultWebView.evaluateJavascript(jsForLocalStorage, new ValueCallback<String>() {
-                  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-                  @Override
-                  public void onReceiveValue(String s) {
-                    JsonReader reader = null;
-                      try {
-                          reader = new JsonReader(new StringReader(s));
-
-                          // Must set lenient to parse single values
-                          reader.setLenient(true);
-
-                          if(reader.peek() != JsonToken.NULL) {
-                              if(reader.peek() == JsonToken.STRING) {
-                                  String msg = reader.nextString();
-                              }
-                          }
-                      } catch (IOException e) {
-                          Log.e("TAG", "MainActivity: IOException", e);
-                      } finally {
-                          try {
-                              reader.close();
-                              cbContext.success("json");
-                          } catch (IOException e) {
-                              // NOOP
-                          }
-                      }
-                  }
-              });
-          } else {
-              /**
-               * For pre-KitKat+ you should use loadUrl("javascript:<JS Code Here>");
-               * To then call back to Java you would need to use addJavascriptInterface()
-               * and have your JS call the interface
-               **/
-              JavaScriptInterface jsInterface = new JavaScriptInterface(activity);
-              defaultWebView.addJavascriptInterface(jsInterface, "JSInterface");
-              defaultWebView.loadUrl("javascript:" + jsForLocalStorage + jsCallback);
-          }
-        } catch (Exception e) {
-
-        }
-        return false;
+        CordovaActivity activity = ((CordovaActivity) this.cordova.getActivity());
+        this.initNow(activity, callbackContext);
+        return true;
     } //Closes execute
 
-    public class JavaScriptInterface {
-        Context mContext;
+    /**
+     * We execute this on the UiThread because of WebView. It gives a lot of warning that it should only be executed on main thread to prevent concurrent problems.
+     */
+    public void initNow(final Activity activity, final CallbackContext callbackContext) {
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                WebView defaultWebView = new WebView(activity);
 
-        /** Instantiate the interface and set the context */
-        JavaScriptInterface(Context c) {
-            mContext = c;
+                final CallbackContext cbContext = callbackContext;
+                try {
+                    String jsCallback = "JSInterface.passBackValues(JSON.stringify(localStorage));";
+
+                    // In KitKat+ you should use the evaluateJavascript method.
+                    // The commented code is kind of a preview of how you could do it. The code itself doesn't work.
+
+                      /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        String function = ""; // http://stackoverflow.com/a/20377857/1587664
+                        defaultWebView.evaluateJavascript(function, new ValueCallback<String>() {
+                          @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+                          @Override
+                          public void onReceiveValue(String s) {
+                            callbackContext.success(s);
+                          }
+                        });
+                      } else {*/
+                    
+                    JavaScriptInterface jsInterface = new JavaScriptInterface(callbackContext);
+                    defaultWebView.addJavascriptInterface(jsInterface, "JSInterface");
+                    defaultWebView.getSettings().setJavaScriptEnabled(true);
+
+                    // Find the correct database path for cordova.
+                    String databasePath = defaultWebView.getContext().getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
+                    defaultWebView.getSettings().setDatabaseEnabled(true);
+                    defaultWebView.getSettings().setDomStorageEnabled(true);
+                    defaultWebView.getSettings().setDatabasePath(databasePath);
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        defaultWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+                        defaultWebView.getSettings().setAllowFileAccessFromFileURLs(true);
+                    }
+
+                    String summary = "<html><body><script>" + jsCallback + "</script></body></html>";
+                    defaultWebView.loadDataWithBaseURL("file:///android_asset/test.html", summary, "text/html", "UTF-8", "about:blank");
+                    //}
+                } catch (Exception e) {
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
+    }
+
+    /**
+     * This is the class that interfaces with javascript.
+     * From javascript we call passBackValues function.
+     */
+    public class JavaScriptInterface {
+        CallbackContext mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        JavaScriptInterface(CallbackContext callbackContext) {
+            mContext = callbackContext;
         }
 
-        public void passBackValues(String items)
-        {
-          Log.e(TAG, "CordovaWebView: We got some localStorage items " + items);
+        @JavascriptInterface
+        public void passBackValues(String items) {
+            mContext.success(items);
         }
     }
 }
